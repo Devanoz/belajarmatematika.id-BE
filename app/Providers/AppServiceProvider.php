e1cfc1a191994cd1f9ea2162854031451ad8316f
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-01-21 18:23:13 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        config(['app.locale' => 'id']);
-	    Carbon::setLocale('id');
+        config(['app.locale' => 'en']);
+	    Carbon::setLocale('en');
     }
 }
